dfb4c47699b4a6f20692db787da064f3a3cfb9fe
Estefany Benítez <azupbr@gmail.com>
2023-12-01 19:39:33 -0600

    prueba

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -12,6 +13,8 @@ class CategoryController extends Controller
     public function index()
     {
         //
+        $category = Category::all();
+        return view('/Empleados/CategoryShow', compact('category'));
     }
 
     /**
@@ -20,6 +23,9 @@ class CategoryController extends Controller
     public function create()
     {
         //
+        $category = Category::all();
+        return view('/Empleados/CategoryShow')->with(['category' => $category]);;
+    
     }
 
     /**
