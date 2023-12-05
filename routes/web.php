662571503ef26cdeb5fd844d3e2bb3189cd23ee3
Estefany Benítez <azupbr@gmail.com>
2023-12-05 12:45:31 -0600

    aceptar cambios-cjr

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ServiceController;
@@ -28,6 +29,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/CategoryShow',[CategoryController::class, 'index']); 
+
 });
 
 // Appointments views
