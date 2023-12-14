11d89721ecdae3abb2e1b78be929a58d7ac63b52
Estefany Benítez <azupbr26@gmail.com>
2023-12-13 20:44:16 -0600

    Update AppointmentShow.blade.php
    prueba

--- resources/views/appointment/AppointmentShow.blade.php
+++ resources/views/appointment/AppointmentShow.blade.php
@@ -6,6 +6,8 @@
 <html>
     <body>
 
+
+        <label for="">CITAS</label>
         <a href="/appointment/create">
             <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >+ Añadir Cita      </button>
         </a>
